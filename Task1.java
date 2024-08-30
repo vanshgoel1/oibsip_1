2af6f452e45ba06394bca314bcc2b3b0d3e90f62
vanshgoel1 <145220084+vanshgoel1@users.noreply.github.com>
2024-08-30 14:09:25 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,70 @@
+package oasis_infobyte;
+import java.util.*;
+
+public class Task1 {
+
+	// Function that implements the
+	// number guessing game
+	public static void main( String args[])
+	{
+		// Scanner Class
+		Scanner sc = new Scanner(System.in);
+
+		// Generate the numbers
+		int number = 1 + (int)(100* Math.random());
+
+		// Given K trials
+		int K = 10;
+
+		int i, guess;
+
+		System.out.println(
+			"A number is chosen"
+			+ " between 1 to 100.\n"
+			+ "Guess the number"
+			+ " within 10 trials.");
+
+		// Iterate over K Trials
+		for (i = 0; i < K; i++) {
+
+			System.out.println(
+				"Guess the number:");
+
+			// Take input for guessing
+			guess = sc.nextInt();
+
+			// If the number is guessed
+			if (number == guess) {
+				System.out.println(
+					"Congratulations!"
+					+ " You guessed the number.");
+				System.out.println("Your score is: " + (K - i) + "out of 10.");
+				break;
+			}
+			else if (number > guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is "
+					+ "greater than " + guess);
+			}
+			else if (number < guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is"
+					+ " less than " + guess);
+			}
+		}
+
+		if (i == K) {
+			System.out.println(
+				"You have exhausted"
+				+ " the number of trials.");
+
+			System.out.println(
+				"The number was " + number);
+		}
+	}
+
+	
+	
+}
